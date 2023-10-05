2795c39fe71c4d3ad1b5fc2f2fc0bfada4ac4a37
JuniorMasete <77290758+Junior-Masete@users.noreply.github.com>
2023-10-05 11:13:32 +0200

    Add files via upload

---
+++ MirroredBinaryTree.java
@@ -0,0 +1,215 @@
+public class MirroredBinaryTree<T extends Comparable<T>> extends BinaryTree<T> {
+    @Override
+    public boolean contains(T data) {
+        if(root == null){
+            return false;
+        }
+        
+        Leaf<T> nnode = contains(root, data);
+        if(nnode != null){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    protected Leaf<T> contains(Leaf<T> node,T data){
+        if(node == null){
+            
+            return null;
+        }
+
+        if(node.data.compareTo(data) == 0){
+            System.out.println(node.toString());
+            return node;
+        }
+        else if(node.data.compareTo(data) > 0){
+            System.out.println(node.toString());
+            return find(node.right, data);
+        }
+        else{
+            System.out.println(node.toString());
+            return find(node.left, data);
+        }
+    }
+
+    @Override
+    public void depthFirstTraversal() {
+        depthFirstTraversal(root);
+    }
+
+    protected void depthFirstTraversal(Leaf<T> node){
+        if(node != null){
+            
+            depthFirstTraversal(node.right);
+            System.out.println(node.toString());
+            node.toString();
+            depthFirstTraversal(node.left);
+        }
+    }
+
+    @Override
+    public int numLeavesInTree() {
+        if(root == null){
+            return 0;
+        }
+        else{
+            return numLeavesInTree(root);
+        }  
+    }
+
+    protected int numLeavesInTree(Leaf<T> node){
+        if(node == null){
+            return 0;
+        }
+        else{
+            return numLeavesInTree(node.left) + numLeavesInTree(node.right) + 1;
+        }
+    }
+
+    @Override
+    public int height() {
+        if(root == null){
+            return 0;
+        }
+        else{
+            return height(root);
+        }
+    }
+
+    protected int height(Leaf<T> node){
+        if(node == null){
+            return -1;
+        }
+        else{
+            int leftH = height(node.left);
+            int rightH = height(node.right);
+
+            if(leftH > rightH){
+                return leftH + 1;
+            }
+            else{
+                return rightH + 1;
+            }
+        }
+    }
+
+    @Override
+    public Leaf<T> findParent(T data) {
+        if(root == null){
+            return null;
+        }
+        else if(root.data.compareTo(data) == 0){
+            //System.out.println("yES I GOT HERE");
+            System.out.println(root.toString());
+            //System.out.println("[" + root.data +"]");
+            return null;
+        }
+        else{
+            Leaf<T> prev = null;
+            findParent(root, prev, data);
+            return prev;
+        }
+    }
+
+    private Leaf<T> findParent(Leaf<T> node,Leaf<T> prev, T data){
+        
+        if(node == null){
+            return null;
+        }
+
+
+        if(node.data.compareTo(data) == 0){
+            //System.out.println(node.toString());
+            return node;
+        }
+        else if(node.data.compareTo(data) > 0){
+            System.out.println(node.toString());
+            prev = node;
+            return findParent(node.right,node, data);
+        }
+        else{
+            System.out.println(node.toString());
+            prev = node;
+            return findParent(node.left,node, data);
+        }
+    }
+
+    @Override
+    public void insert(T data) {
+        super.insert(data, false);
+    }
+
+    @Override
+    public Leaf<T> find(T data) {
+        if(root == null){
+            return null;
+        }
+        else{
+            return find(root, data);
+        }
+    }
+
+    protected Leaf<T> find(Leaf<T> node, T data){
+        if(node == null){
+            //System.out.println(node.toString());
+            return null;
+        }
+
+        if(node.data.compareTo(data) == 0){
+            System.out.println(node.toString());
+            return node;
+        }
+        else if(node.data.compareTo(data) > 0){
+            System.out.println(node.toString());
+            return find(node.right, data);
+        }
+        else{
+            System.out.println(node.toString());
+            return find(node.left, data);
+        }
+
+
+    }
+
+    @Override
+    public boolean perfectlyBalanced() {
+        Leaf<T> leftOfroot = root.left;
+        Leaf<T> rigthtOfroot = root.right;
+        int lh = numLeavesInTree(leftOfroot);
+        int rh = numLeavesInTree(rigthtOfroot);
+        if(lh == rh){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    @Override
+    public BinaryTree<T> convertTree() {
+        if(root == null){
+            return null;
+        }
+        else{
+            BinaryTree<T> tr = new StandardBinaryTree<>();
+            tr.root = root;
+            mirror(tr.root);
+            return tr;
+        }
+    }
+
+    private Leaf<T> mirror(Leaf<T> node){
+        if(node == null){
+            return null;
+        }
+
+        Leaf<T> left = mirror(node.left);
+        Leaf<T> right = mirror(node.right);
+
+        node.left = right;
+        node.right = left;
+        return node;
+    }
+}
